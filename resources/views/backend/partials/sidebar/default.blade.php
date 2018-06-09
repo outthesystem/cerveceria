b9e8415bb1cbaa3c56546019525add0cf2200991
outthesystem <jet.market.dev@gmail.com>
2018-06-09 14:51:45 -0300

    fix sum model

--- resources/views/backend/partials/sidebar/default.blade.php
+++ resources/views/backend/partials/sidebar/default.blade.php
@@ -28,7 +28,7 @@
           <div class="content-header-item">
             <a class="link-effect font-w700" href="{{url('/')}}">
                             <i class="si si-moustache text-primary"></i>
-                            <span class="font-size-xl text-dual-primary-dark">gentle</span><span class="font-size-xl text-primary">Man</span>
+                            <span class="font-size-xl text-dual-primary-dark">gentle</span><span class="font-size-xl text-primary">Men</span>
                         </a>
           </div>
           <!-- END Logo -->
